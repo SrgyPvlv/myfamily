b5c20789a246a70210dcbae5b5149adcff576e62
Sergey Pavlov <s.a.pavlov@mail.ru>
2023-02-17 17:09:59 +0300

    test

--- src/main/java/com/example/family/controller/HelloController.java
+++ src/main/java/com/example/family/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//test
+
 @RestController
 public class HelloController {
 
